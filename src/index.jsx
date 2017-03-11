864029f17d38620ac13d1fb37adcf8fb9252f3be
jeff christian <jchri@MacBook-Pro-2.loca>
2017-03-11 15:50:58 -0800

    rebased

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< d64f0674ed1a2f7804022f4b702c9d8d0c3f5ebb
 import RecognitionV from './component/RecognitionV';
-import './css/index.css';
-
-class Index extends Component {
-	constructor (props) {
-    super(props)
-    this.state = {
-    	showText: true
-    };
-  }
-
-=======
 import { VoicePlayer } from 'react-voice-components'
 import axios from 'axios';
 import './css/index.css';
@@ -26,7 +14,7 @@ class Index extends Component {
   }
   handleSubmit(){
     var val = this.state.text
-    axios.get('http://localhost:3000/resturant', {
+    axios.get('http://localhost:3000/restaurant/list', {
       headers: {
         'Access-Control-Allow-Origin': true
       }
@@ -34,7 +22,6 @@ class Index extends Component {
     .then(data => console.log('asdfasdfasd', data))
     .catch( err => { throw new Error(err)})
   }
->>>>>>> changes
 	render() {
 	  return (
 		<div>
@@ -45,10 +32,8 @@ class Index extends Component {
           </section>
         </header>
         <section className="searchbar">
-<<<<<<< d64f0674ed1a2f7804022f4b702c9d8d0c3f5ebb
           <form>
             <input className="search" type="text" placeholder="Search for dining"/>
-=======
           <form onSubmit={(e) => {
             e.preventDefault()
             console.log(this.state.text)
@@ -65,7 +50,6 @@ class Index extends Component {
               type='submit' 
               value='submit' 
             />
->>>>>>> changes
           </form>
         </section>
         <section className="content-contai
