ce4cad62dbb96573153eee210a52fcd468f0224f
Cyril <loki0119@gmail.com>
2017-03-11 15:33:56 -0800

    add restaurant route api

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const restaurant = require('./routes/restaurant');
+
 app.use(express.static('dist'))
+
+app.use('/restaurant', restaurant);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
