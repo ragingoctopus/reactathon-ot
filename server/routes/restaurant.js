78dfd2501fe9abc4f0decf1de113f8b0fc77c722
Cyril <loki0119@gmail.com>
2017-03-11 16:13:45 -0800

    finished implement search restaurant api

--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -2,24 +2,43 @@ var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
 
-router.get('/list', (req, res) => {
-  console.log('LLLLLLL')
+const API = {
+  SEARCH_ALL: '/searchall',
+  SEARCH: '/search/:id'
+}
+
+const URLs = {
+  SEARCH: 'https://platform.otqa.com/availability'
+}
+
+router.get(API.SEARCH, (req, res) => {
+  const spec = req.query
+  const rid = req.params.id
+
   const options = {
-    uri: 'https://platform.otqa.com/availability/334879?start_date_time=2017-03-29T18%3A00&party_size=2&forward_minutes=120&backward_minutes=30',
-    method: 'POST',
+    uri: `${URLs.SEARCH}/${rid}`,
+    method: 'GET',
     headers: {
-        Authorization: 'bearer 27037c67-f394-4cfd-ab51-069ac71132fb'
+      Authorization: 'bearer 27037c67-f394-4cfd-ab51-069ac71132fb'
     },
-    json: true
+    qs: {
+      start_date_time: spec.start_date_time,
+      forward_minutes: spec.forward_minutes,
+      backward_minutes: spec.backward_minutes,
+      party_size: spec.party_size
+    }
   }
+
   rp(options)
     .then(response => {
-      console.log(response)
-      return res.status(200).send(response)
+      if (response) {
+        return res.status(200).send(response)
+      } else {
+        return res.status(200).send('No restaurant matched.')
+      }
     })
     .catch(err => {
-      console.log(err)
-      return res.send(400)
+      return res.statusCode(500).send('Research restaurant fail.')
     })
 })
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const restaurant = require('./routes/restaurant');
 
 app.use(express.static('dist'))
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
+
 app.use('/restaurant', restaurant);
 
 app.listen(port, () => {
