d9f1dc66e92379d5e889e8f2c3b2babdf5573bb5
Andrew <andrew@Andrews-MacBook-Pro.local>
2017-03-11 16:10:59 -0800

    Add voice player

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import RecognitionV from './component/RecognitionV';
+import VoicePlayer from './lib/VoicePlayer';
 import './css/index.css';
 
 class Index extends Component {
 	constructor (props) {
     super(props)
     this.state = {
-    	showText: true
+    	showText: true,
+    	playVoice: false,
+    	voiceText: '',
     };
+  }
+
+  voiceResult(result) {
+  	console.log("Result: " + result);
+  	this.setState({ voiceText:result }, () => {
+  		this.setState({ playVoice: true });
+  	});
+  }
+
+  onEnd() {
+  	this.setState({ playVoice: false });
   }
 
 	render() {
@@ -16,20 +30,24 @@ class Index extends Component {
 		<div>
 			<main className="main-app-container">
         <header className="content-container header">
-          <section>
             <img src="http://res.cloudinary.com/meetshermanchen-com/image/upload/v1489262593/Logo_horizontal_RGB_rrnfhk.png" alt="OpenTable" className="brand"/>
-          </section>
         </header>
         <section className="searchbar">
           <form>
             <input className="search" type="text" placeholder="Search for dining"/>
           </form>
         </section>
-        <section className="content-contai
-        ner speech-button">
-            <p><i className="fa fa-microphone fa-5x icon"></i></p>
+        <section className="content-container speech-button">
+        	<RecognitionV voiceResult={this.voiceResult.bind(this)}/>
+        	{this.state.playVoice && (
+        		<VoicePlayer 
+        	  play
+    				text={this.state.voiceText}
+    				onEnd={this.onEnd.bind(this)}
+    				/>
+    			)}
         </section>
-  			<RecognitionV />
+  			
       </main>
 		</div>
 	  )
